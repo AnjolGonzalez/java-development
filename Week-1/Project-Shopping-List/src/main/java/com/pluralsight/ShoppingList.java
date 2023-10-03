82452bfc7fa13a4a20189bf77870c050786c0586
Student <agonzalez000@appdev.yearup.org>
2023-10-03 15:13:19 -0400

    Complete

---
+++ Week-1/Project-Shopping-List/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Bread, Case of water, Cereal, Milk, Eggs, Strawberry CreamCheese, Spicy Turbos, Turkey, Cheese, Foils");
+    }
+
+}
